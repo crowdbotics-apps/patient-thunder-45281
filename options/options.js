c80687dc45ba2f88efb09bddc73f722c50035e8c
Crowdbotics <team@crowdbotics.com>
2024-01-04 21:36:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "patient-thunder-45281",
+  url: "https://patient-thunder-45281.botics.co",
+  api: "https://patient-thunder-45281.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
